b05ece74d92a4ea88957c2ab26d7488b9732f0e1
Ashley Phillips <ashley.phillips@nodeocrat.com>
2018-01-22 21:08:24 +0000

    terminate socket connection upon app close

--- index.js
+++ index.js
@@ -5,20 +5,35 @@ import {Provider} from 'react-redux';
 import store from './store';
 import {Route, Switch} from 'react-router-dom';
 import {appStarted} from './actions/userActions';
+import {Rooms} from 'client-room';
 
-const NodeSocial = props => {
-  store.dispatch(appStarted(props.user));
+export default class NodeSocial extends React.Component {
+  constructor(props){
+    super(props);
+    store.dispatch(appStarted(props.user));
+  }
 
-  return (
-    <section>
-      <Provider store={store}>
-        <Switch>
-          <Route path={props.match.path} exact component={Lobby} />
-          <Route path={`${props.match.path}/game`} component={Game} />
-        </Switch>
-      </Provider>
-    </section>
-  );
+  render(){
+    return (
+      <section>
+        <Provider store={store}>
+          <Switch>
+            <Route path={this.props.match.path} exact component={Lobby} />
+            <Route path={`${this.props.match.path}/game`} component={Game} />
+          </Switch>
+        </Provider>
+      </section>
+    );
+  }
+
+  componentWillUnmount(){
+
+    //Remove this (terrible code) once clietn room auto socket close imeplemented
+    let last = true;
+    for(let [roomId, room] of Rooms){
+      room.leave();
+      if(last)
+        room._socket.close();
+    }
+  }
 };
-
-export default NodeSocial;
